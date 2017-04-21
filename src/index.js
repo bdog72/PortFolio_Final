24f2e0b27f84cebc65acbed83c6b1037310bfaf3
Brian Bycynski <high5bri@hotmail.com>
2017-04-21 05:50:06 -0400

    cleaned up a few files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import Redbox from 'redbox-react'
 
-import App from './components/App'
+// import App from './components/App'
+import Grid from './components/Grid'
 
 import './styles/screen.scss'
 import './styles/grid.scss'
@@ -15,12 +16,19 @@ const render = app => {
     root
   )
 }
+render(<Grid />)
 
-render(<App />)
+// render(<App />)
 
+// if (module.hot) {
+//   module.hot.accept('./components/App', () => {
+//     const NextApp = require('./components/App').default
+//     render(<NextApp />)
+//   })
+// }
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    const NextApp = require('./components/App').default
+  module.hot.accept('./components/Grid', () => {
+    const NextApp = require('./components/Grid').default
     render(<NextApp />)
   })
 }
